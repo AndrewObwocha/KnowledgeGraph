66147279ddd8fcffef1f11eb35c6361ecefe00e4
Andrew Obwocha <zeros-faddish.0t@icloud.com>
2025-08-27 15:32:36 -0600

    feat: Created Navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
+import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
-    null
+    <Navbar/>
   )
 }
 
-export default App
+export default App;
