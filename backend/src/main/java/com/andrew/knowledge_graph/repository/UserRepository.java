855d1929b7b14bf1e56192e257537b197e25debb
Andrew Obwocha <zeros-faddish.0t@icloud.com>
2025-09-08 22:46:20 -0600

    feat: Created User repository to handle search logic

---
+++ backend/src/main/java/com/andrew/knowledge_graph/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.andrew.knowledge_graph.repository;
+
+import com.andrew.knowledge_graph.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
